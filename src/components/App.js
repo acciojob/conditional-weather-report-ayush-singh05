50f85935c70c359d426d5e2557e6c70434179ee8
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-12-23 05:03:52 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,14 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const weather = {temperature: 25, conditions: "Sunny" };
+  const tempClass = weather.temperature > 20 ? 'above':'below'; 
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>temperature:  <span className={tempClass}>{weather.temperature}</span></p>
+        <p>Conditions: {weather.conditions}</p>
     </div>
   )
 }
